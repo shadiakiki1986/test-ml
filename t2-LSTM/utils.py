3fd61d4737f5b59e1bf3d08a312f4bdb913288fb
Shadi Akiki <shadiakiki1986@gmail.com>
2017-08-07 14:17:55 +0000

    allow symmetric vs non-symmetric  AE and experimenting further with tensorboard

--- t2-LSTM/utils.py
+++ t2-LSTM/utils.py
@@ -56,7 +56,7 @@ def buildNetwork(input_shape:int, encoding_dim_ae:int=2):
     
     return (autoencoder, encoder)
 
-def buildNetwork2_deep(input_shape:int, enc_dim1:int, enc_dim2:int=None, enc_dim3:int=None, enc_dim4:int=None):
+def buildNetwork2_deep(input_shape:int, enc_dim1:int, enc_dim2:int=None, enc_dim3:int=None, enc_dim4:int=None, symmetric:bool=True):
     if enc_dim2 is None and (enc_dim3 is not None or enc_dim4 is not None):
       raise Exception("dim2 is None but dim3 or dim4 is not None")
     if enc_dim3 is None and enc_dim4 is not None:
@@ -74,19 +74,24 @@ def buildNetwork2_deep(input_shape:int, enc_dim1:int, enc_dim2:int=None, enc_dim
     encoded = LeakyReLU(alpha=.3)(encoded)   # add an advanced activation
 
     # GET DEEP
+    decoded = encoded
     if enc_dim2 is not None:
       encoded = Dense(enc_dim2, activation='linear')(encoded)
       encoded = LeakyReLU(alpha=.3)(encoded)
+      decoded = encoded
       if enc_dim3 is not None:
         encoded = Dense(enc_dim3, activation='linear')(encoded)
         encoded = LeakyReLU(alpha=.3)(encoded)
+        decoded = encoded
         if enc_dim4 is not None:
           encoded = Dense(enc_dim4, activation='linear')(encoded)
           encoded = LeakyReLU(alpha=.3)(encoded)
-          decoded = Dense(enc_dim3, activation='relu')(encoded)
-        decoded = Dense(enc_dim2, activation='relu')(encoded)
-      decoded = Dense(enc_dim1, activation='relu')(encoded)
-    decoded = Dense(input_shape, activation='sigmoid')(encoded)
+          decoded = encoded
+          # ...
+          if symmetric: decoded = Dense(enc_dim3, activation='relu')(decoded)
+        if symmetric: decoded = Dense(enc_dim2, activation='relu')(decoded)
+      if symmetric: decoded = Dense(enc_dim1, activation='relu')(decoded)
+    decoded = Dense(input_shape, activation='sigmoid')(decoded)
 
     autoencoder = Model(input_img, decoded)
     encoder = Model(input_img, encoded)
