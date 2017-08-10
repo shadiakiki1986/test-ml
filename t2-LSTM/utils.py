6cc3511271dbdd4bfcd47237f02f0b8f1352f003
Shadi Akiki <shadiakiki1986@gmail.com>
2017-08-10 11:52:52 +0300

    prepare p5g copy 6

--- t2-LSTM/utils.py
+++ t2-LSTM/utils.py
@@ -56,7 +56,7 @@ def buildNetwork(input_shape:int, encoding_dim_ae:int=2):
     
     return (autoencoder, encoder)
 
-def buildNetwork2_deep(input_shape:int, enc_dim1:int, enc_dim2:int=None, enc_dim3:int=None, enc_dim4:int=None, symmetric:bool=True):
+def buildNetwork2_deep(input_shape:int, enc_dim1:int, enc_dim2:int=None, enc_dim3:int=None, enc_dim4:int=None, symmetric:bool=True, optimizer='rmsprop'):
     if enc_dim2 is None and (enc_dim3 is not None or enc_dim4 is not None):
       raise Exception("dim2 is None but dim3 or dim4 is not None")
     if enc_dim3 is None and enc_dim4 is not None:
@@ -101,8 +101,8 @@ def buildNetwork2_deep(input_shape:int, enc_dim1:int, enc_dim2:int=None, enc_dim
     # decoder = Model(encoded_input, decoder_layer(encoded_input))
 
     # other: optimizer='adadelta', loss='binary_crossentropy'
-    autoencoder.compile(optimizer='rmsprop', loss='mean_squared_error')
-    encoder.compile(optimizer='rmsprop', loss='mean_squared_error')
+    autoencoder.compile(optimizer=optimizer, loss='mean_squared_error')
+    encoder.compile(optimizer=optimizer, loss='mean_squared_error')
     # decoder.compile(optimizer='rmsprop', loss='mean_squared_error')
     
     return (autoencoder, encoder)
