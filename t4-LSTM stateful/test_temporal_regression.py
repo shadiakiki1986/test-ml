1f0ebecd0e369d8de381168a60e9c83916acfb39
Shadi Akiki <shadiakiki1986@gmail.com>
2017-09-20 02:58:22 +0000

    minor

--- t4-LSTM stateful/test_temporal_regression.py
+++ t4-LSTM stateful/test_temporal_regression.py
@@ -11,7 +11,7 @@ import timeit
 def doit(epochs,batch_size,layer_type):
 	start_time = timeit.default_timer()
 
-	np.random.seed(1337)
+#	np.random.seed(1337)
 	(x_train, y_train), (x_test, y_test) = get_test_data(num_train=2000,
 							 num_test=200,
 							 input_shape=(5,) if layer_type=='Dense' else (3, 5),
@@ -25,22 +25,41 @@ def doit(epochs,batch_size,layer_type):
 	else:
 		model.add(layers.LSTM(y_train.shape[-1],
 				  input_shape=(x_train.shape[1], x_train.shape[2])))
-	model.compile(loss='hinge', optimizer='adam')
+	model.compile(loss='mse', optimizer='adam') # hinge
 	history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size,
 			validation_data=(x_test, y_test), verbose=0)
 	print(
-		'epochs %s, batch size %s, layer type %s: final loss %s,              seconds %s'
+		'epochs %-5s, batch size %-5s, layer type %-10s: final loss %0.2f, seconds %0.2f'
 		%(
 		epochs,      batch_size,     layer_type,     history.history['loss'][-1], timeit.default_timer() - start_time
 		)
 	)
 
+"""
+	np.random.seed(1338)
+	(x_train, y_train), (x_test, y_test) = get_test_data(num_train=2000,
+							 num_test=200,
+							 input_shape=(5,) if layer_type=='Dense' else (3, 5),
+							 output_shape=(2,),
+							 classification=False)
+	score_train = model.evaluate(x_train, y_train, verbose=0)
+	score_test = model.evaluate(x_test, y_test, verbose=0)
+	score_train_1 = model.evaluate(x_train[:1000 ], y_train[:1000 ], verbose=0)
+	score_train_2 = model.evaluate(x_train[ 1000:], y_train[ 1000:], verbose=0)
+
+	print('%0.2f, %0.2f, %0.2f, %0.2f' % (score_train, score_test, score_train_1, score_train_2))
+"""
 
 doit( 15, 16,'Dense')
-doit(150,160,'Dense')
 doit( 15,160,'Dense')
+doit(150,160,'Dense')
 
-doit( 15, 16,'Lstm')
-doit(150,160,'Lstm')
-doit( 15,160,'Lstm')
+"""
+doit( 15, 20,'Dense')
+doit( 15,200,'Dense')
+doit(150,200,'Dense')
 
+doit( 15, 20,'Lstm')
+doit( 15,200,'Lstm')
+doit(150,200,'Lstm')
+"""
